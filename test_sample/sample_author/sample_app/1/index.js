3470b982126cd7ab768c0287e9f91737e137da7e
Matthew Kropp <matt@prmtv.com>
2018-07-08 21:48:12 -0700

    stuff

---
+++ test_sample/sample_author/sample_app/1/index.js
@@ -0,0 +1,5 @@
+const { add, interval, subtract, count } = require('../../../../test/sample_author/sample_app/1/src/math');
+const { updateProfile, getProfile } = require('../../../../test/sample_author/sample_app/1/src/profle');
+
+module.exports = { add, interval, subtract, count, updateProfile, getProfile };
+
