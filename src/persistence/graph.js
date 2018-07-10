c36c74215e42872f7d8b963c304f47d8b68c422f
Matthew Kropp <matt@prmtv.com>
2018-07-09 22:29:06 -0700

    Add Relate and FindByRelation to graph

--- src/persistence/graph.js
+++ src/persistence/graph.js
@@ -8,7 +8,7 @@ class Graph {
         this.db = levelgraph(level(dbPath));
     }
 
-    remember(objectType, objectName, signature, object) {
+    remember(objectType, objectName, signature, object, relation) {
         return new Promise((fulfill, reject) => {
             let subject = `${objectName}@${signature}`;
             let batch = [];
@@ -18,6 +18,11 @@ class Graph {
                 let triple = {subject: subject, predicate: key, object: object[key]};
                 batch.push(triple);
             });
+
+            if (relation && relation.predicate && relation.object) {
+                let relationTriple = { subject: subject, predicate: relation.predicate, object: relation.object };
+                batch.push(relationTriple);
+            }
 
             this.db.put(batch, err => {
                 if (err)
@@ -30,13 +35,41 @@ class Graph {
     }
 
     relate(subjectKey, predicate, objectKey) {
+        return  new Promise((fulfill, reject) => {
+            let triple = {subject: subjectKey, predicate: predicate, object: objectKey};
+            this.db.put(triple, err => {
+                if (err)
+                    reject(err);
+                else
+                    fulfill();
+            });
+        });
+    }
 
+    findByRelation({subject, predicate, object}) {
+        return new Promise((fulfill, reject) => {
+            if (((subject ? 1 : 0) + (predicate ? 1 : 0) + (object ? 1 : 0)) < 2)
+                reject(new Error('FindByRelation requires at least 2 of subject, predicate, object'));
+
+            let triple = {};
+            if (subject) triple.subject = subject;
+            if (predicate) triple.predicate = predicate;
+            if (object) triple.object = object;
+
+            this.db.get(triple, function process(err, list) {
+                if (err) reject(err);
+
+                fulfill(list);
+            });
+        });
     }
 
     recall(key) {
         return new Promise((fulfill, reject) => {
             let obj = {};
             this.db.get({subject: key}, (err, list) => {
+                if (err) reject(err);
+
                 list.map(triple => {
                     obj[triple.predicate] = triple.object;
                 });
--- src/persistence/graph.test.js
+++ src/persistence/graph.test.js
@@ -4,8 +4,9 @@ const assert = chai.assert;
 const Graph = require('./graph');
 
 describe('graph', function(done) {
+    let graph = new Graph();
+
     it('should store an object and retrieve it', function(done) {
-        let graph = new Graph();
         graph.remember('#Person', 'Matthew Kropp', '4031760169', {email: 'matt@gmail.com', phone: '415-555-1234'})
             .then(r => {
                 graph.recall('Matthew Kropp@4031760169')
@@ -14,6 +15,17 @@ describe('graph', function(done) {
                         done();
                     })
             })
+    });
 
-    })
-})
+    it('should create a relation and find related objects', function(done) {
+        graph.remember('#Person', 'Karen Rosenquist', '4031760170', {email: 'karen@gmail.com', phone: '310-555-1234'},
+            {predicate: 'wifeOf', object: 'Matthew Kropp@4031760169'})
+            .then(r => {
+                graph.findByRelation({predicate: 'wifeOf', object: 'Matthew Kropp@4031760169'})
+                    .then(result => {
+                        assert.equal(result[0].subject, 'Karen Rosenquist@4031760170');
+                        done();
+                    })
+            })
+    });
+});
